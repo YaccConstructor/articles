cd63eb541120512c909a3780ceda164c15d10483
Dmitry Boulytchev <dboulytchev@gmail.com>
2019-08-16 21:19:39 +0300

    Hanoi + new syntax

--- InProgress/RelInterpreter/slides/examples/hanoi/build.sh
+++ InProgress/RelInterpreter/slides/examples/hanoi/build.sh
@@ -1,2 +1,2 @@
-ocamlfind c -rectypes -o hanoi -package ocanren -package GT -syntax camlp5o -package GT.syntax.all -package ocanren.syntax -linkpkg hanoi.ml
+ocamlfind c -rectypes -o hanoi -package ocanren -package GT -syntax "camlp5o pr_o.cmo" -package GT.syntax.all -package ocanren.syntax -linkpkg hanoi.ml
 
