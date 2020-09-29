1d158460f71b82a519e4bea3f1df657fe9d35cf3
gsv <rsdpisuy@gmail.com>
2020-09-29 11:11:29 +0300

    Multiple-source matrix-based CFPQ. New figures for small chunks.

--- InProgress/The_RedisGraph_Strikes_Back/data/draw.py
+++ InProgress/The_RedisGraph_Strikes_Back/data/draw.py
@@ -13,7 +13,7 @@ files_to_draw = [f'raw/{x}.csv' for x in ['core','eclass_514en','enzyme','geospe
 files_to_draw_redis = [f'raw_redis/{x}.csv' for x in ['core','eclass_514en','enzyme','geospecies','go','gohierarchy','pathways']]
 
 
-def draw(input_file, p):
+def draw(input_file, p=0):
 
 	labels = []
 	def add_label(violin, label):
@@ -27,20 +27,13 @@ def draw(input_file, p):
 	
 	fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(6, 4))
 
-	#plt.xscale('log')
-	#plt.yscale('log')
-
 	i = 0
 	for name, group in data:
-		#print(name)
-		data=group[['chunk_size','chunk_time']].groupby(['chunk_size'])
-		#print(data)
+		data = group[['chunk_size','chunk_time']].groupby(['chunk_size'])
+		if p > 0 : data = list(data)[:p]
 		r = [x for x in [[n,reject_outliers(d['chunk_time'])] for n,d in data] if len(x[1]) > 0 ]
 		d = list(zip(*r))
-		#print(r)
 		axs = axes[int (i/2)]
-		#axs.ticklabel_format(axis='y',style='sci')
-		#axs.set_yscale('log')
 		if i%2 == 0 : labels = []
 		add_label(axs.violinplot(d[1],
 				       showmeans=False,
@@ -50,8 +43,6 @@ def draw(input_file, p):
 		axs.set_xticklabels(d[0])
 		
 		i = i + 1
-		#axs.set_xlabel('Chunk size')
-		#axs.set_ylabel('Time in sec')
 		axs.yaxis.grid(True)
 
 		leg = axs.legend(*zip(*labels), loc=2);
@@ -62,18 +53,13 @@ def draw(input_file, p):
 
 	fig.tight_layout()
 
-	plt.savefig(f'{input_file[:-4]}.pdf')
+	f_name = f'{input_file[:-4]}'
+	if p > 0 : f_name = f'{f_name}_{p}'
+	plt.savefig(f'{f_name}.pdf')
 	plt.close(fig)
-	#plt.show()
 
 
 def draw_redis(input_file, p):
-
-	labels = []
-	def add_label(violin, label):
-	    color = violin["bodies"][0].get_facecolor().flatten()
-	    labels.append((mpatches.Patch(color=color), label))
-
 
 	plt.ioff()
 
@@ -86,14 +72,11 @@ def draw_redis(input_file, p):
 	r = [x for x in [[n,reject_outliers(d['chunk_time'])] for n,d in data] if len(x[1]) > 0 ]
 	d = list(zip(*r))
 	axs = axes
-#	if i%2 == 0 : labels = []
 	axs.violinplot(d[1], showmeans=False, showmedians=True)
 
 	axs.set_xticks(range(1,len(d[0])+1))
 	axs.set_xticklabels(d[0])
 	
-	#axs.set_xlabel('Chunk size')
-	#axs.set_ylabel('Time in sec')
 	axs.yaxis.grid(True)
 
 	leg = axs.legend([input_data['grammar'][0]], loc=2);
@@ -106,7 +89,5 @@ def draw_redis(input_file, p):
 
 	plt.savefig(f'{input_file[:-4]}.pdf')
 	plt.close(fig)
-	#plt.show()
 
-
-for f in files_to_draw_redis: draw_redis(f,10)
+for f in files_to_draw: draw(f,4)
